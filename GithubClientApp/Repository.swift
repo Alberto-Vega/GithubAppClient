ef015c0165b4a67d97ed77c518b9f2689413efb4
Alberto-Vega <el_vega@live.com>
2015-11-13 18:26:35 -0800

    Initial Commit

---
+++ GithubClientApp/Repository.swift
@@ -0,0 +1,9 @@
+//
+//  Repository.swift
+//  GithubClientApp
+//
+//  Created by Alberto Vega Gonzalez on 11/13/15.
+//  Copyright © 2015 Alberto Vega Gonzalez. All rights reserved.
+//
+
+import Foundation
